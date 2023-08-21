e3f7d508797352b8e900e88f37892faf28bf93b9
Mahan Mashoof <mahan.mashoof@gmail.com>
2023-08-21 16:58:51 -0300

    title, portions, and ingredients design ok

---
+++ src/components/Recipe/Recipe.styles.ts
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  font-family: "Roboto", sans-serif;
+`;
+
+export const NameRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 1rem auto 2rem;
+  align-items: center;
+`;
+
+export const MealName = styled.h1`
+  margin: 0;
+  font-size: 1.5rem;
+`;
+
+export const PortionsGroup = styled.div`
+  padding-left: 2rem;
+  text-align: center;
+`;
+
+export const PortionSizeButton = styled.button`
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
+  font-weight: bold;
+  border: none;
+  font-size: 1.5rem;
+`;
+
+export const PortionSizeGroup = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const PortionsInput = styled.input`
+  width: 2rem;
+  border: none;
+  text-align: center;
+  font-size: 1.5rem;
+`;
+
+export const IngredientsRow = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0.5rem auto;
+`;
+
+export const IngredientIcon = styled.img`
+  width: 10%;
+`;
+
+export const IngredientName = styled.div`
+  text-align: left;
+  padding-left: 1rem;
+  width: 60%;
+`;
+
+export const IngredientQuantity = styled.div`
+  text-align: right;
+  padding-left: 1rem;
+  width: 15%;
+`;
--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -1,10 +1,88 @@
-import Header from "../Header/Header";
+import * as S from "./Recipe.styles";
 
 function Recipe() {
   return (
-    <div>
-      <Header />
-    </div>
+    <S.Main>
+      <img
+        src="https://static01.nyt.com/images/2009/04/14/health/14recipehealth_600/14recipehealth_600-articleLarge.jpg?w=1280&q=75"
+        alt=""
+        style={{ width: "100%", aspectRatio: "16/9" }}
+      />
+      <S.NameRow>
+        <S.MealName>Spinach, Tofu and Sesame Stir-Fry</S.MealName>
+        <S.PortionsGroup>
+          <div style={{ marginBottom: "0.5rem" }}>portions:</div>
+          <S.PortionSizeGroup>
+            <S.PortionSizeButton style={{ backgroundColor: "#cfe0c3" }}>
+              -
+            </S.PortionSizeButton>
+            <S.PortionsInput defaultValue={3} />
+            <S.PortionSizeButton
+              style={{ backgroundColor: "#70a9a1", color: "#fff" }}
+            >
+              +
+            </S.PortionSizeButton>
+          </S.PortionSizeGroup>
+        </S.PortionsGroup>
+      </S.NameRow>
+      <S.IngredientsRow>
+        <S.IngredientIcon src="/img/canola-oil.png" alt="canola oil" />
+        <S.IngredientName>Canola oil</S.IngredientName>
+        <S.IngredientQuantity>1</S.IngredientQuantity>
+        <S.IngredientQuantity>tbs</S.IngredientQuantity>
+      </S.IngredientsRow>
+      <S.IngredientsRow>
+        <S.IngredientIcon src="/img/tofu.png" alt="tofu" />
+        <S.IngredientName>Tofu, small dices</S.IngredientName>
+        <S.IngredientQuantity>0.5</S.IngredientQuantity>
+        <S.IngredientQuantity>lb</S.IngredientQuantity>
+      </S.IngredientsRow>
+      <S.IngredientsRow>
+        <S.IngredientIcon src="/img/garlic.png" alt="garlic" />
+        <S.IngredientName>Garlic, minced</S.IngredientName>
+        <S.IngredientQuantity>1</S.IngredientQuantity>
+        <S.IngredientQuantity>clove</S.IngredientQuantity>
+      </S.IngredientsRow>
+      <S.IngredientsRow>
+        <S.IngredientIcon src="/img/ginger.png" alt="ginger" />
+        <S.IngredientName>Ginger</S.IngredientName>
+        <S.IngredientQuantity>1</S.IngredientQuantity>
+        <S.IngredientQuantity>tsp</S.IngredientQuantity>
+      </S.IngredientsRow>
+      <S.IngredientsRow>
+        <S.IngredientIcon src="/img/chili.png" alt="chili flakes" />
+        <S.IngredientName>Chili flakes</S.IngredientName>
+        <S.IngredientQuantity>0.25</S.IngredientQuantity>
+        <S.IngredientQuantity>tsp</S.IngredientQuantity>
+      </S.IngredientsRow>
+      <S.IngredientsRow>
+        <S.IngredientIcon src="/img/soy-sauce.png" alt="soy sauce" />
+        <S.IngredientName>Soy sauce</S.IngredientName>
+        <S.IngredientQuantity>-</S.IngredientQuantity>
+        <S.IngredientQuantity>taste</S.IngredientQuantity>
+      </S.IngredientsRow>
+      <S.IngredientsRow>
+        <S.IngredientIcon src="/img/spinach.png" alt="spinach" />
+        <S.IngredientName>Spinach</S.IngredientName>
+        <S.IngredientQuantity>6</S.IngredientQuantity>
+        <S.IngredientQuantity>oz</S.IngredientQuantity>
+      </S.IngredientsRow>
+      <S.IngredientsRow>
+        <S.IngredientIcon src="/img/sesame.png" alt="sesame seeds" />
+        <S.IngredientName>Sesame seeds, toasted</S.IngredientName>
+        <S.IngredientQuantity>2</S.IngredientQuantity>
+        <S.IngredientQuantity>tbs</S.IngredientQuantity>
+      </S.IngredientsRow>
+      <S.IngredientsRow>
+        <S.IngredientIcon src="/img/sesame-oil.png" alt="sesame oil" />
+        <S.IngredientName>Sesame oil</S.IngredientName>
+        <S.IngredientQuantity>1</S.IngredientQuantity>
+        <S.IngredientQuantity>tbs</S.IngredientQuantity>
+      </S.IngredientsRow>
+      <p>recipe step x</p>
+      <p>step progress bar</p>
+      <p>footer w options</p>
+    </S.Main>
   );
 }
 
