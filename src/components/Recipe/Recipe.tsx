2162aca9af4cc06fb42f3bad65aec68e5744eb94
Mahan Mashoof <mahan.mashoof@gmail.com>
2023-08-21 17:11:54 -0300

    minor style updates

--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -13,7 +13,7 @@ function Recipe() {
         <S.PortionsGroup>
           <div style={{ marginBottom: "0.5rem" }}>portions:</div>
           <S.PortionSizeGroup>
-            <S.PortionSizeButton style={{ backgroundColor: "#cfe0c3" }}>
+            <S.PortionSizeButton style={{ backgroundColor: "#9ec1a3" }}>
               -
             </S.PortionSizeButton>
             <S.PortionsInput defaultValue={3} />
@@ -25,60 +25,62 @@ function Recipe() {
           </S.PortionSizeGroup>
         </S.PortionsGroup>
       </S.NameRow>
-      <S.IngredientsRow>
-        <S.IngredientIcon src="/img/canola-oil.png" alt="canola oil" />
-        <S.IngredientName>Canola oil</S.IngredientName>
-        <S.IngredientQuantity>1</S.IngredientQuantity>
-        <S.IngredientQuantity>tbs</S.IngredientQuantity>
-      </S.IngredientsRow>
-      <S.IngredientsRow>
-        <S.IngredientIcon src="/img/tofu.png" alt="tofu" />
-        <S.IngredientName>Tofu, small dices</S.IngredientName>
-        <S.IngredientQuantity>0.5</S.IngredientQuantity>
-        <S.IngredientQuantity>lb</S.IngredientQuantity>
-      </S.IngredientsRow>
-      <S.IngredientsRow>
-        <S.IngredientIcon src="/img/garlic.png" alt="garlic" />
-        <S.IngredientName>Garlic, minced</S.IngredientName>
-        <S.IngredientQuantity>1</S.IngredientQuantity>
-        <S.IngredientQuantity>clove</S.IngredientQuantity>
-      </S.IngredientsRow>
-      <S.IngredientsRow>
-        <S.IngredientIcon src="/img/ginger.png" alt="ginger" />
-        <S.IngredientName>Ginger</S.IngredientName>
-        <S.IngredientQuantity>1</S.IngredientQuantity>
-        <S.IngredientQuantity>tsp</S.IngredientQuantity>
-      </S.IngredientsRow>
-      <S.IngredientsRow>
-        <S.IngredientIcon src="/img/chili.png" alt="chili flakes" />
-        <S.IngredientName>Chili flakes</S.IngredientName>
-        <S.IngredientQuantity>0.25</S.IngredientQuantity>
-        <S.IngredientQuantity>tsp</S.IngredientQuantity>
-      </S.IngredientsRow>
-      <S.IngredientsRow>
-        <S.IngredientIcon src="/img/soy-sauce.png" alt="soy sauce" />
-        <S.IngredientName>Soy sauce</S.IngredientName>
-        <S.IngredientQuantity>-</S.IngredientQuantity>
-        <S.IngredientQuantity>taste</S.IngredientQuantity>
-      </S.IngredientsRow>
-      <S.IngredientsRow>
-        <S.IngredientIcon src="/img/spinach.png" alt="spinach" />
-        <S.IngredientName>Spinach</S.IngredientName>
-        <S.IngredientQuantity>6</S.IngredientQuantity>
-        <S.IngredientQuantity>oz</S.IngredientQuantity>
-      </S.IngredientsRow>
-      <S.IngredientsRow>
-        <S.IngredientIcon src="/img/sesame.png" alt="sesame seeds" />
-        <S.IngredientName>Sesame seeds, toasted</S.IngredientName>
-        <S.IngredientQuantity>2</S.IngredientQuantity>
-        <S.IngredientQuantity>tbs</S.IngredientQuantity>
-      </S.IngredientsRow>
-      <S.IngredientsRow>
-        <S.IngredientIcon src="/img/sesame-oil.png" alt="sesame oil" />
-        <S.IngredientName>Sesame oil</S.IngredientName>
-        <S.IngredientQuantity>1</S.IngredientQuantity>
-        <S.IngredientQuantity>tbs</S.IngredientQuantity>
-      </S.IngredientsRow>
+      <div style={{ backgroundColor: "#f6f6f6", padding: "0.5rem 0" }}>
+        <S.IngredientsRow>
+          <S.IngredientIcon src="/img/canola-oil.png" alt="canola oil" />
+          <S.IngredientName>Canola oil</S.IngredientName>
+          <S.IngredientQuantity>1</S.IngredientQuantity>
+          <S.IngredientQuantity>tbs</S.IngredientQuantity>
+        </S.IngredientsRow>
+        <S.IngredientsRow>
+          <S.IngredientIcon src="/img/tofu.png" alt="tofu" />
+          <S.IngredientName>Tofu, small dices</S.IngredientName>
+          <S.IngredientQuantity>0.5</S.IngredientQuantity>
+          <S.IngredientQuantity>lb</S.IngredientQuantity>
+        </S.IngredientsRow>
+        <S.IngredientsRow>
+          <S.IngredientIcon src="/img/garlic.png" alt="garlic" />
+          <S.IngredientName>Garlic, minced</S.IngredientName>
+          <S.IngredientQuantity>1</S.IngredientQuantity>
+          <S.IngredientQuantity>clove</S.IngredientQuantity>
+        </S.IngredientsRow>
+        <S.IngredientsRow>
+          <S.IngredientIcon src="/img/ginger.png" alt="ginger" />
+          <S.IngredientName>Ginger</S.IngredientName>
+          <S.IngredientQuantity>1</S.IngredientQuantity>
+          <S.IngredientQuantity>tsp</S.IngredientQuantity>
+        </S.IngredientsRow>
+        <S.IngredientsRow>
+          <S.IngredientIcon src="/img/chili.png" alt="chili flakes" />
+          <S.IngredientName>Chili, flakes</S.IngredientName>
+          <S.IngredientQuantity>0.25</S.IngredientQuantity>
+          <S.IngredientQuantity>tsp</S.IngredientQuantity>
+        </S.IngredientsRow>
+        <S.IngredientsRow>
+          <S.IngredientIcon src="/img/soy-sauce.png" alt="soy sauce" />
+          <S.IngredientName>Soy sauce</S.IngredientName>
+          <S.IngredientQuantity>-</S.IngredientQuantity>
+          <S.IngredientQuantity>taste</S.IngredientQuantity>
+        </S.IngredientsRow>
+        <S.IngredientsRow>
+          <S.IngredientIcon src="/img/spinach.png" alt="spinach" />
+          <S.IngredientName>Spinach</S.IngredientName>
+          <S.IngredientQuantity>6</S.IngredientQuantity>
+          <S.IngredientQuantity>oz</S.IngredientQuantity>
+        </S.IngredientsRow>
+        <S.IngredientsRow>
+          <S.IngredientIcon src="/img/sesame.png" alt="sesame seeds" />
+          <S.IngredientName>Sesame seeds, toasted</S.IngredientName>
+          <S.IngredientQuantity>2</S.IngredientQuantity>
+          <S.IngredientQuantity>tbs</S.IngredientQuantity>
+        </S.IngredientsRow>
+        <S.IngredientsRow>
+          <S.IngredientIcon src="/img/sesame-oil.png" alt="sesame oil" />
+          <S.IngredientName>Sesame oil</S.IngredientName>
+          <S.IngredientQuantity>1</S.IngredientQuantity>
+          <S.IngredientQuantity>tbs</S.IngredientQuantity>
+        </S.IngredientsRow>
+      </div>
       <p>recipe step x</p>
       <p>step progress bar</p>
       <p>footer w options</p>
